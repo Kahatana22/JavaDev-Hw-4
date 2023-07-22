4f4f022514a391d5ec489e875fa45c4a42dd9851
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-07-22 15:08:29 +0300

    Delete delet.sql

--- src/main/resources/sql/delet.sql
+++
@@ -1,5 +0,0 @@
--- SELECT id AS name, DATEDIFF(month, start_date, finish_date) AS project_count
--- FROM project
--- WHERE DATEDIFF(month, start_date, finish_date) = (
---     SELECT MAX(DATEDIFF(month, start_date, finish_date)) FROM project
--- );
